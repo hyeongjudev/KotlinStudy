1c3ece67538140596ac7a5ab3a6ebe318b73b68b
HyeongJu Kim <kinghj.kim@gmail.com>
2019-02-02 22:51:21 +0900

    Second commit

--- src/FirstTest.kt
+++ src/FirstTest.kt
@@ -1,3 +1,20 @@
 fun main(args: Array<String>) {
+    println("종결자(;) 없어도 됩니다.")
+    println("var는 읽기쓰기. val닌 읽기전용")
 
+    var nCount : Int = 0
+    var name  = "psw"
+
+    val age = 0
+    //age++
+
+    var MyMoney : Int? = null
+
+    //println(message)
+
+    var message = "위에서는 액세스 불가함"
+
+    println(hiMessage)
 }
+
+val hiMessage : String = "Hi"
